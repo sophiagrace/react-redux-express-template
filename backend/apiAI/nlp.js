0351fdf2ab8e48471704013942dc69ee3813d2b7
Amanda <ajhwhiz@gmail.com>
2017-08-07 10:54:35 -0700

    added nlp.js, sendQuery function

---
+++ backend/apiAI/nlp.js
@@ -0,0 +1,35 @@
+// NPM Imports
+const axios = require('axios');
+
+// Necessary Tokens
+const API_URL_BASE = 'https://api.api.ai/v1/';
+const CLIENT_ACCESS_TOKEN = process.env.API_CLIENT_ACCESS_TOKEN || '';
+
+console.log('client: ', CLIENT_ACCESS_TOKEN);
+
+// Global Variables
+const api_ai = axios.create({
+  baseURL: API_URL_BASE,
+  headers: {'Authorization': 'Bearer ' + CLIENT_ACCESS_TOKEN}
+});
+
+// Local Helper Function
+// Generates the url to sent to API.AI
+function generateQueryString(query, id) {
+  console.log('got query string', query);
+  return '/query?v=20150910&lang=en&sessionId=' + id + '&query=' + encodeURIComponent(query);
+}
+
+// Exported Function
+// nlp.sendQuery(query, slackId)
+//  - Param: widgetID -> String
+//           query   -> String
+//  - Description: Send an API call to API.AI
+//    using the query string and slackId specified
+function sendQuery(query, widgetID) {
+  return api_ai.get(generateQueryString(query, widgetID))
+}
+
+module.exports = {
+  sendQuery
+}
